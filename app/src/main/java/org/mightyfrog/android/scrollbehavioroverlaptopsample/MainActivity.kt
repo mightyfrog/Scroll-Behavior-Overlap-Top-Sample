2c7e89664adb8ccc7b7274e53d04389dc8d4ea68
mightyfrog <mightyfrog.gc@gmail.com>
2017-10-17 22:50:28 +0900

    Initial commit

---
+++ app/src/main/java/org/mightyfrog/android/scrollbehavioroverlaptopsample/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.mightyfrog.android.scrollbehavioroverlaptopsample
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.graphics.Palette
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * @author Shigehiro Soejima
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val p = Palette.from(BitmapFactory.decodeResource(resources, R.drawable.pizza)).generate()
+        findViewById<View>(android.R.id.content).setBackgroundColor(p.dominantSwatch?.rgb!!)
+    }
+}
